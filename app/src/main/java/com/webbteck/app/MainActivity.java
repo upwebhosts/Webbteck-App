a73924e947913104f743e1f1b16a129aee4a6469
upwebhosts <upwebhosts@gmail.com>
2026-09-24 00:26:09 +0100

    Implement WebView to load external website

---
+++ app/src/main/java/com/webbteck/app/MainActivity.java
@@ -0,0 +1,43 @@
+package com.webbteck.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        // Keep navigation inside the app instead of launching external browser
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load your website
+        webView.loadUrl("https://webbteck.com");
+    }
+
+    // Handle back button press to navigate backwards in web history
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
